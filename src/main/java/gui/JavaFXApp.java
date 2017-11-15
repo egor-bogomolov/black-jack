8ef6d0d8fa0098edd42e383d32a930e8ac975383
niksaz <nikitasazanovich@gmail.com>
2017-11-15 15:17:24 +0300

    Resized button based on the window size.

--- src/main/java/gui/JavaFXApp.java
+++ src/main/java/gui/JavaFXApp.java
@@ -1,9 +1,12 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 public class JavaFXApp extends Application {
@@ -34,10 +37,16 @@ public class JavaFXApp extends Application {
         passButton = new Button("PASS");
         takeButton.setOnAction(e -> field.playerTurnPass());
 
+        HBox buttonHorizontalBox = new HBox(10, takeButton, passButton);
+        buttonHorizontalBox.setAlignment(Pos.CENTER);
+        HBox.setHgrow(takeButton, Priority.ALWAYS);
+        HBox.setHgrow(passButton, Priority.ALWAYS);
+        takeButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        passButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
         rootLayout = new BorderPane();
+        rootLayout.setTop(buttonHorizontalBox);
         rootLayout.setCenter(field);
-        rootLayout.setLeft(takeButton);
-        rootLayout.setRight(passButton);
 
         Scene scene = new Scene(rootLayout, MIN_WIDTH, MIN_HEIGHT);
         primaryStage.setScene(scene);
