1575e86acf11d5d9d00fb00a30618b355403a0b7
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2017-11-15 15:10:11 +0300

    Implement APIClient Fixes #5

--- src/main/java/game/APIClient.java
+++ src/main/java/game/APIClient.java
@@ -2,15 +2,45 @@ package game;
 
 import cards.Card;
 
-import java.io.Serializable;
+import java.io.*;
+import java.net.Socket;
 
-public interface APIClient {
-    enum Operation implements Serializable {
+public class APIClient {
+    public static final int PORT = 6789;
+
+    public enum Operation implements Serializable {
         TAKE,
         PASS
     }
+    private Socket socket;
+    private ObjectOutputStream os;
+    private ObjectInputStream is;
 
-    Card take();
-    void pass();
-    State nextEvent();
+    /**
+     * Connects to the Server via TCP. Returns the initial state of the game.
+     */
+    public State connect() throws IOException, ClassNotFoundException {
+        socket = new Socket("localhost", PORT);
+        os = new ObjectOutputStream(socket.getOutputStream());
+        is = new ObjectInputStream(socket.getInputStream());
+        return nextEvent();
+    }
+
+    public void close() throws IOException {
+        socket.close();
+    }
+
+    public State take() throws IOException, ClassNotFoundException {
+        os.writeObject(Operation.TAKE);
+        return nextEvent();
+    }
+
+    public State pass() throws IOException {
+        os.writeObject(Operation.PASS);
+        return nextEvent();
+    }
+
+    State nextEvent() throws IOException, ClassNotFoundException {
+        return (State)is.readObject();
+    }
 }
